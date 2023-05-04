99ab54aa664061cfc35b3aa1d3ad7ae9f05aed8c
rennavittorio <renna.vittorio@gmail.com>
2023-05-04 19:18:55 +0200

    add: add feat to not query the project selected in the related projects

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class ProjectController extends Controller
 {
@@ -19,7 +21,13 @@ class ProjectController extends Controller
 
     public function show($slug)
     { //questo slug lo riceve dalla call client
-        $project = Project::with('technologies.projects')->where('slug', $slug)->first();
+        // $project = Project::where('slug', $slug)->first();
+        // $project->load('technologies.projects');
+        $project = Project::where('slug', $slug)->with(['technologies.projects' => function ($query) use ($slug) {
+            //no BUilder, solo $query dentro la =>function() (da docs Laravel https://laravel.com/docs/9.x/eloquent-relationships#eager-loading)
+            //use serve per portare sotto la variabile (vedi thread laracast https://laracasts.com/discuss/channels/eloquent/eager-load-relation-with-parameters)
+            $query->where('slug', '!=', $slug);
+        }])->first();
 
         //controlliamo se esiste il post
         if ($project) {
