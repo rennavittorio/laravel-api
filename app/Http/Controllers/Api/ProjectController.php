6e1940130eaa1e5abc2e1b73fcc002308b61c528
rennavittorio <renna.vittorio@gmail.com>
2023-05-04 15:13:32 +0200

    add: add api route for project detail

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,4 +16,22 @@ class ProjectController extends Controller
             'projects' => $projects,
         ]);
     }
+
+    public function show($slug)
+    { //questo slug lo riceve dalla call client
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+        //controlliamo se esiste il post
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'nessun project trovato',
+            ]);
+        }
+    }
 }
